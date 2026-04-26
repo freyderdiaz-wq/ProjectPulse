169439a0950078984d935022265c56f8f215e6e3
Operator Developer <operator@trycore.com>
2026-04-26 01:41:00 -0500

    feat: integración de EvmService en actividades y pruebas unitarias

--- back_end/src/actividades/actividades.module.ts
+++ back_end/src/actividades/actividades.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ActividadesService } from './actividades.service';
 import { ActividadesController } from './actividades.controller';
 
+import { EvmService } from '../evm/evm.service';
+
 @Module({
   controllers: [ActividadesController],
-  providers: [ActividadesService],
+  providers: [ActividadesService, EvmService],
+  exports: [EvmService],
 })
 export class ActividadesModule {}
--- back_end/src/actividades/actividades.service.ts
+++ back_end/src/actividades/actividades.service.ts
@@ -1,10 +1,21 @@
+
 import { Injectable } from '@nestjs/common';
 import { CreateActividadeDto } from './dto/create-actividade.dto';
 import { UpdateActividadeDto } from './dto/update-actividade.dto';
+import { EvmService } from '../evm/evm.service';
 
 @Injectable()
 export class ActividadesService {
+  constructor(private readonly evmService: EvmService) {}
+
   create(createActividadeDto: CreateActividadeDto) {
+    // Ejemplo de uso:
+    // const evmResult = this.evmService.calculateEvmIndicators({
+    //   bac: createActividadeDto.bac,
+    //   plannedProgressPercent: createActividadeDto.porcentajeAvancePlanificado,
+    //   actualProgressPercent: createActividadeDto.porcentajeAvanceReal,
+    //   ac: createActividadeDto.costoActual,
+    // });
     return 'This action adds a new actividade';
   }
 
